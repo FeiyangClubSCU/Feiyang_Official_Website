bddd539ba15a1ff307bdf701bfcd6f7c0bd53e39
皮卡丘 <pikachuim@qq.com>
2020-10-11 14:13:32 +0800

    UPDATE-202010100000

---
+++ newpage/email.php
@@ -0,0 +1,15 @@
+<?php
+    $mail_host='smtp.qq.com';
+    $mail_user='';				  //邮箱账号
+    $mail_pass='';				  //邮箱密码
+    $mail_port=587;
+    $mail_from='';                //邮箱账号
+    $mail_name='飞扬俱乐部';      //发信机构
+    $mail_dbug='飞扬官网';		  //发信称呼
+    $selt_alls='';				  //默认地址
+    $selt_wxbm='';				  //维修部门
+    $selt_yfbm='';				  //研发部门
+    $selt_xzbm='';				  //行政部门
+    $selt_bjbm='';				  //编辑部门
+    $selt_sjbm='';				  //设计部门
+?>
